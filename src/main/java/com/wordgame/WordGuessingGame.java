fdc36eaf2810f509c3c76c8b8ce477bb16b85162
waynelu93 <waynelu93@users.noreply.github.com>
2026-05-27 23:02:47 -0700

    Init Java Project

---
+++ src/main/java/com/wordgame/WordGuessingGame.java
@@ -0,0 +1,7 @@
+package com.wordgame;
+
+public class WordGuessingGame {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Word Guessing Game!");
+    }
+}
---
+++ src/test/java/com/wordgame/WordGuessingGameTest.java
@@ -0,0 +1,11 @@
+package com.wordgame;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class WordGuessingGameTest {
+    @Test
+    public void testGameInitialization() {
+        assertNotNull(new Object());
+    }
+}
